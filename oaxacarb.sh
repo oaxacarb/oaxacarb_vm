8a12483e951a3712cb9590122375224d54d01474
thotmx <hermes.o.r@gmail.com>
2016-08-15 12:58:32 -0500

    Fix IP address for VM and add simple script
      * The script install local packages
      * Install RVM on the VM

---
+++ oaxacarb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install locale packages
+sudo apt-get -y install language-pack-es
+
+# Install RVM
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
